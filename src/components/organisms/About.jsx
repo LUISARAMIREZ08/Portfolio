0f11ce68c63ce1f5703ddccc53358a8633461a59
Luisa Ramirez <luisa.ramirez3@utp.edu.co>
2025-05-24 17:08:38 -0500

    Responsive Design

--- src/components/organisms/About.jsx
+++ src/components/organisms/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
             <SectionTitle Icon={AboutMe} title="Sobre mí" />
             <div className='container-text-img-about'>
                 <p className='text-about'>
-                  ¡Hola! Soy Luisa, estudiante de Ingeniería de Sistemas en la Universidad Tecnológica de Pereira y con formación en desarrollo Full-Stack.  Me encanta crear soluciones con código,  desarrollar paginas web con interfaces llamativas y funcionales.  Soy una persona organizada, creativa y autidacta, me encanta aprender y estar actualizada de los avances de la tecnológia cada dia. 
+                  ¡Hola! Soy Luisa, Ingeniería de Sistemas de la Universidad Tecnológica de Pereira y con formación en desarrollo Full-Stack.  Me encanta crear soluciones con código,  desarrollar paginas web con interfaces llamativas y funcionales.  Soy una persona organizada, creativa y autidacta, me encanta aprender y estar actualizada de los avances de la tecnológia cada dia. 
                 </p>
                 <div className='container-img-about'>
                     <img src={PersonAbout} alt="person" className='img-about'/>

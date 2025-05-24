ea308f61b79df0ea2e40a37f6baea64481e47650
Luisa Ramirez <luisa.ramirez3@utp.edu.co>
2025-05-24 18:38:39 -0500

    The cursor on mobile screens has been modified

--- src/components/utils/GradientCursor.jsx
+++ src/components/utils/GradientCursor.jsx
@@ -1,10 +1,24 @@
 import { useState, useEffect } from 'react';
-import '../../styles/gradientCursor.css'; 
+import '../../styles/gradientCursor.css';
 
 const GradientCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Detects if the device is touch-enabled
+    const mediaQuery = window.matchMedia('(pointer: coarse)');
+    setIsMobile(mediaQuery.matches);
+
+    const updateMatch = () => setIsMobile(mediaQuery.matches);
+    mediaQuery.addEventListener('change', updateMatch);
+
+    return () => mediaQuery.removeEventListener('change', updateMatch);
+  }, []);
+
+  useEffect(() => {
+    if (isMobile) return;
+
     const handleMouseMove = (e) => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
@@ -14,7 +28,9 @@ const GradientCursor = () => {
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
     };
-  }, []);
+  }, [isMobile]);
+
+  if (isMobile) return null;
 
   return (
     <div 
